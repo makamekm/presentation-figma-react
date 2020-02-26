c02f30df055cb6459ca92192e6ee3792979e9aa4
makame <makame@mail.ru>
2020-02-26 22:48:42 +0100

    fix font position

--- src/design-system/frame1.generated.tsx
+++ src/design-system/frame1.generated.tsx
@@ -22,95 +22,44 @@ export const Frame1Generated: React.FC<{}> = observer(props => {
             </div>
           </div>
           <div className="figma-14">
-            <div id="5:3" className="figma-13">
+            <div id="4:31" className="figma-13">
+              <div
+                className="vector figma-12"
+                dangerouslySetInnerHTML={{
+                  __html: `<svg preserveAspectRatio="none" width="124" height="185" viewBox="0 0 124 185" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0)">
+<path d="M124 31C124 13.8792 110.121 0 93 0H32C14.8792 0 1 13.8792 1 31C1 48.1208 14.8792 62 32 62H93C110.121 62 124 48.1208 124 31Z" fill="#F2321E"/>
+<path d="M124 31C124 13.8792 110.121 0 93 0H63V62H93C110.121 62 124 48.1208 124 31Z" fill="#FF6C5C"/>
+<circle r="30.5" transform="matrix(-1 0 0 1 93.5 92.5)" fill="#1ABDFF"/>
+<path d="M31.4626 185C14.4338 185 0.629257 171.195 0.629257 154.167C0.629257 137.138 14.4338 123.333 31.4626 123.333H62.2959V154.167C62.2959 171.195 48.4914 185 31.4626 185Z" fill="#3ED67E"/>
+<path d="M30.8333 61.6667H62.2959V123.333H30.8333C13.8046 123.333 0 109.529 0 92.5C0 75.4712 13.8046 61.6667 30.8333 61.6667C30.8334 61.6667 30.8333 61.6667 30.8333 61.6667Z" fill="#943FFF"/>
+</g>
+<defs>
+<clipPath id="clip0">
+<rect width="124" height="185" fill="white"/>
+</clipPath>
+</defs>
+</svg>
+`
+                }}
+              />
+            </div>
+          </div>
+          <div className="figma-17">
+            <div id="4:41" className="figma-16"></div>
+          </div>
+          <div className="figma-21">
+            <div id="5:3" className="figma-20">
               <span key="end">+</span>
             </div>
           </div>
-          <div className="figma-18">
-            <div id="5:4" className="figma-17">
+          <div className="figma-25">
+            <div id="5:4" className="figma-24">
               <span key="end">=</span>
             </div>
           </div>
-          <div className="figma-21">
-            <div id="4:31" className="figma-20">
-              <div className="figma-25">
-                <div id="4:32" className="figma-24">
-                  <div
-                    className="vector figma-23"
-                    dangerouslySetInnerHTML={{
-                      __html: `<svg preserveAspectRatio="none" width="123" height="62" viewBox="0 0 123 62" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M123 31C123 13.8792 109.121 0 92 0H31C13.8792 0 0 13.8792 0 31C0 48.1208 13.8792 62 31 62H92C109.121 62 123 48.1208 123 31Z" fill="#F2321E"/>
-</svg>
-`
-                    }}
-                  />
-                </div>
-              </div>
-              <div className="figma-29">
-                <div id="4:33" className="figma-28">
-                  <div
-                    className="vector figma-27"
-                    dangerouslySetInnerHTML={{
-                      __html: `<svg preserveAspectRatio="none" width="61" height="62" viewBox="0 0 61 62" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M61 31C61 13.8792 47.1208 0 30 0H0V62H30C47.1208 62 61 48.1208 61 31Z" fill="#FF6C5C"/>
-</svg>
-`
-                    }}
-                  />
-                </div>
-              </div>
-              <div className="figma-33">
-                <div id="4:39" className="figma-32">
-                  <div
-                    className="vector figma-31"
-                    dangerouslySetInnerHTML={{
-                      __html: `<svg preserveAspectRatio="none" width="62" height="62" viewBox="0 0 62 62" fill="none" xmlns="http://www.w3.org/2000/svg">
-<circle r="30.8333" transform="matrix(-1.00541 0 0 1.00541 31 31)" fill="#C4C4C4"/>
-</svg>
-`
-                    }}
-                  />
-                </div>
-              </div>
-              <div className="figma-36">
-                <div id="4:40" className="figma-35"></div>
-              </div>
-              <div className="figma-40">
-                <div id="4:34" className="figma-39">
-                  <div
-                    className="vector figma-38"
-                    dangerouslySetInnerHTML={{
-                      __html: `<svg preserveAspectRatio="none" width="61" height="61" viewBox="0 0 61 61" fill="none" xmlns="http://www.w3.org/2000/svg">
-<circle r="30.5" transform="matrix(-1 0 0 1 30.5 30.5)" fill="#1ABDFF"/>
-</svg>
-`
-                    }}
-                  />
-                </div>
-              </div>
-              <div className="figma-44">
-                <div id="4:36" className="figma-43">
-                  <div
-                    className="vector figma-42"
-                    dangerouslySetInnerHTML={{
-                      __html: `<svg preserveAspectRatio="none" width="62" height="62" viewBox="0 0 62 62" fill="none" xmlns="http://www.w3.org/2000/svg">
-<circle r="30.8333" transform="matrix(-1.00541 0 0 1.00541 31 31)" fill="#C4C4C4"/>
-</svg>
-`
-                    }}
-                  />
-                </div>
-              </div>
-              <div className="figma-47">
-                <div id="4:37" className="figma-46"></div>
-              </div>
-            </div>
-          </div>
-          <div className="figma-50">
-            <div id="4:41" className="figma-49"></div>
-          </div>
-          <div className="figma-53">
-            <div id="5:2" className="figma-52"></div>
+          <div className="figma-28">
+            <div id="5:2" className="figma-27"></div>
           </div>
         </div>
       </div>
@@ -172,10 +121,10 @@ export const Frame1Generated: React.FC<{}> = observer(props => {
           letter-spacing: 0px;
           display: flex;
           max-width: -webkit-fill-available;
-          align-content: flex-start;
+          align-content: center;
           justify-content: center;
-          vertical-align: top;
-          align-items: flex-start;
+          vertical-align: middle;
+          align-items: center;
         }
         .figma-5 {
           position: relative;
@@ -220,10 +169,10 @@ export const Frame1Generated: React.FC<{}> = observer(props => {
           letter-spacing: 0px;
           display: flex;
           max-width: -webkit-fill-available;
-          align-content: flex-start;
+          align-content: center;
           justify-content: center;
-          vertical-align: top;
-          align-items: flex-start;
+          vertical-align: middle;
+          align-items: center;
           opacity: 0.6000000238418579;
         }
         .figma-10 {
@@ -237,78 +186,6 @@ export const Frame1Generated: React.FC<{}> = observer(props => {
           position: relative;
           box-sizing: border-box;
           pointer-events: auto;
-          z-index: 1;
-          debug-h: CENTER;
-          width: 61px;
-          margin-left: -491px;
-          debug-v: TOP;
-          margin-top: -336px;
-          margin-bottom: 165px;
-          min-height: 171px;
-          color: rgba(255, 255, 255, 1);
-          font-size: 144px;
-          font-weight: 400;
-          text-transform: uppercase;
-          font-family: Yanone Kaffeesatz;
-          text-align: CENTER;
-          font-style: normal;
-          line-height: undefined%;
-          letter-spacing: 0px;
-          display: flex;
-          max-width: -webkit-fill-available;
-          align-content: flex-start;
-          justify-content: center;
-          vertical-align: top;
-          align-items: flex-start;
-        }
-        .figma-14 {
-          position: relative;
-          display: flex;
-          width: 100%;
-          pointer-events: none;
-          justify-content: center;
-          z-index: 1;
-        }
-        .figma-17 {
-          position: relative;
-          box-sizing: border-box;
-          pointer-events: auto;
-          z-index: 2;
-          debug-h: CENTER;
-          width: 61px;
-          margin-left: 29px;
-          debug-v: TOP;
-          margin-top: -336px;
-          margin-bottom: 165px;
-          min-height: 171px;
-          color: rgba(255, 255, 255, 1);
-          font-size: 144px;
-          font-weight: 400;
-          text-transform: uppercase;
-          font-family: Yanone Kaffeesatz;
-          text-align: CENTER;
-          font-style: normal;
-          line-height: undefined%;
-          letter-spacing: 0px;
-          display: flex;
-          max-width: -webkit-fill-available;
-          align-content: flex-start;
-          justify-content: center;
-          vertical-align: top;
-          align-items: flex-start;
-        }
-        .figma-18 {
-          position: relative;
-          display: flex;
-          width: 100%;
-          pointer-events: none;
-          justify-content: center;
-          z-index: 2;
-        }
-        .figma-20 {
-          position: relative;
-          box-sizing: border-box;
-          pointer-events: auto;
           z-index: 4;
           debug-h: CENTER;
           width: 124px;
@@ -317,11 +194,8 @@ export const Frame1Generated: React.FC<{}> = observer(props => {
           margin-top: -326px;
           margin-bottom: 141px;
           min-height: 185px;
-          overflow: hidden;
-          background: linear-gradient(to bottom, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0) 100%);
-          background-size: auto;
         }
-        .figma-21 {
+        .figma-14 {
           position: relative;
           display: flex;
           width: 100%;
@@ -329,148 +203,7 @@ export const Frame1Generated: React.FC<{}> = observer(props => {
           justify-content: center;
           z-index: 4;
         }
-        .figma-24 {
-          position: relative;
-          box-sizing: border-box;
-          pointer-events: auto;
-          debug-h: LEFT;
-          width: 123px;
-          margin-left: 1px;
-          min-width: 123px;
-          debug-v: TOP;
-          margin-top: 0px;
-          margin-bottom: 123px;
-          min-height: 62px;
-        }
-        .figma-25 {
-          position: relative;
-          display: flex;
-        }
-        .figma-28 {
-          position: relative;
-          box-sizing: border-box;
-          pointer-events: auto;
-          z-index: 1;
-          debug-h: LEFT;
-          width: 61px;
-          margin-left: 63px;
-          min-width: 61px;
-          debug-v: TOP;
-          margin-top: -185px;
-          margin-bottom: 123px;
-          min-height: 62px;
-        }
-        .figma-29 {
-          position: relative;
-          display: flex;
-          z-index: 1;
-          pointer-events: none;
-        }
-        .figma-32 {
-          position: relative;
-          box-sizing: border-box;
-          pointer-events: auto;
-          z-index: 6;
-          debug-h: LEFT;
-          width: 61.66666793823242px;
-          margin-left: -0.00000762939453125px;
-          min-width: 61.66666793823242px;
-          debug-v: TOP;
-          margin-top: -123.33331298828125px;
-          margin-bottom: 61.66664505004883px;
-          min-height: 61.66666793823242px;
-        }
-        .figma-33 {
-          position: relative;
-          display: flex;
-          z-index: 6;
-          pointer-events: none;
-        }
-        .figma-35 {
-          position: relative;
-          box-sizing: border-box;
-          pointer-events: auto;
-          z-index: 7;
-          debug-h: LEFT;
-          width: 31.46258544921875px;
-          margin-left: 30.833343505859375px;
-          min-width: 31.46258544921875px;
-          debug-v: TOP;
-          margin-top: -123.33331298828125px;
-          margin-bottom: 61.66664505004883px;
-          min-height: 61.66666793823242px;
-          background: linear-gradient(to bottom, rgba(162, 89, 255, 1) 0%, rgba(162, 89, 255, 1) 100%);
-          background-size: auto;
-        }
-        .figma-36 {
-          position: relative;
-          display: flex;
-          z-index: 7;
-          pointer-events: none;
-        }
-        .figma-39 {
-          position: relative;
-          box-sizing: border-box;
-          pointer-events: auto;
-          z-index: 2;
-          debug-h: LEFT;
-          width: 61px;
-          margin-left: 63px;
-          min-width: 61px;
-          debug-v: TOP;
-          margin-top: -123px;
-          margin-bottom: 62px;
-          min-height: 61px;
-        }
-        .figma-40 {
-          position: relative;
-          display: flex;
-          z-index: 2;
-          pointer-events: none;
-        }
-        .figma-43 {
-          position: relative;
-          box-sizing: border-box;
-          pointer-events: auto;
-          z-index: 3;
-          debug-h: LEFT;
-          width: 61.66666793823242px;
-          margin-left: 0.6292572021484375px;
-          min-width: 61.66666793823242px;
-          debug-v: TOP;
-          margin-top: -61.66668701171875px;
-          margin-bottom: 0.000019073486328125px;
-          min-height: 61.66666793823242px;
-        }
-        .figma-44 {
-          position: relative;
-          display: flex;
-          z-index: 3;
-          pointer-events: none;
-        }
-        .figma-46 {
-          position: relative;
-          box-sizing: border-box;
-          pointer-events: auto;
-          z-index: 4;
-          debug-h: LEFT;
-          width: 30.83333396911621px;
-          margin-left: 31.46259307861328px;
-          min-width: 30.83333396911621px;
-          debug-v: TOP;
-          margin-top: -61.66668701171875px;
-          margin-bottom: 30.83335304260254px;
-          min-height: 30.83333396911621px;
-          background: linear-gradient(to bottom, rgba(196, 196, 196, 1) 0%, rgba(196, 196, 196, 1) 100%);
-          background-size: auto;
-        }
-        .figma-47 {
-          position: relative;
-          display: flex;
-          z-index: 4;
-          pointer-events: none;
-        }
-        .figma-49 {
+        .figma-16 {
           position: relative;
           box-sizing: border-box;
           pointer-events: auto;
@@ -485,7 +218,7 @@ export const Frame1Generated: React.FC<{}> = observer(props => {
           background: url(/images/4-41.png) center center no-repeat;
           background-size: cover;
         }
-        .figma-50 {
+        .figma-17 {
           position: relative;
           display: flex;
           width: 100%;
@@ -493,7 +226,79 @@ export const Frame1Generated: React.FC<{}> = observer(props => {
           justify-content: center;
           z-index: 5;
         }
-        .figma-52 {
+        .figma-20 {
+          position: relative;
+          box-sizing: border-box;
+          pointer-events: auto;
+          z-index: 1;
+          debug-h: CENTER;
+          width: 85px;
+          margin-left: -491px;
+          debug-v: TOP;
+          margin-top: -315px;
+          margin-bottom: 149px;
+          min-height: 166px;
+          color: rgba(255, 255, 255, 1);
+          font-size: 144px;
+          font-weight: 400;
+          text-transform: uppercase;
+          font-family: Arial;
+          text-align: CENTER;
+          font-style: normal;
+          line-height: undefined%;
+          letter-spacing: 0px;
+          display: flex;
+          max-width: -webkit-fill-available;
+          align-content: center;
+          justify-content: center;
+          vertical-align: middle;
+          align-items: center;
+        }
+        .figma-21 {
+          position: relative;
+          display: flex;
+          width: 100%;
+          pointer-events: none;
+          justify-content: center;
+          z-index: 1;
+        }
+        .figma-24 {
+          position: relative;
+          box-sizing: border-box;
+          pointer-events: auto;
+          z-index: 2;
+          debug-h: CENTER;
+          width: 85px;
+          margin-left: 29px;
+          debug-v: TOP;
+          margin-top: -315px;
+          margin-bottom: 149px;
+          min-height: 166px;
+          color: rgba(255, 255, 255, 1);
+          font-size: 144px;
+          font-weight: 400;
+          text-transform: uppercase;
+          font-family: Arial;
+          text-align: CENTER;
+          font-style: normal;
+          line-height: undefined%;
+          letter-spacing: 0px;
+          display: flex;
+          max-width: -webkit-fill-available;
+          align-content: center;
+          justify-content: center;
+          vertical-align: middle;
+          align-items: center;
+        }
+        .figma-25 {
+          position: relative;
+          display: flex;
+          width: 100%;
+          pointer-events: none;
+          justify-content: center;
+          z-index: 2;
+        }
+        .figma-27 {
           position: relative;
           box-sizing: border-box;
           pointer-events: auto;
@@ -508,7 +313,7 @@ export const Frame1Generated: React.FC<{}> = observer(props => {
           background: url(/images/5-2.png) center center no-repeat;
           background-size: cover;
         }
-        .figma-53 {
+        .figma-28 {
           position: relative;
           display: flex;
           width: 100%;
@@ -517,19 +322,7 @@ export const Frame1Generated: React.FC<{}> = observer(props => {
           z-index: 6;
         }
 
-        .figma-23 > :global(svg) {
-        }
-
-        .figma-27 > :global(svg) {
-        }
-
-        .figma-31 > :global(svg) {
-        }
-
-        .figma-38 > :global(svg) {
-        }
-
-        .figma-42 > :global(svg) {
+        .figma-12 > :global(svg) {
         }
       `}</style>
     </>
